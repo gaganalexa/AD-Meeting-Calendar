60b9f03d1045567098ec0ae9b7ebeadc89982e3c
gaganalexa <202310537@fit.edu.ph>
2025-07-08 21:21:59 +0800

    refactor(user.model.sql): redefine users table structure with UUID and additional fields

--- database/user.model.sql
+++ database/user.model.sql
@@ -1,8 +1,11 @@
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(100) NOT NULL UNIQUE,
-    password TEXT NOT NULL,
-    email VARCHAR(150) UNIQUE,
-    role VARCHAR(50),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+DROP TABLE IF EXISTS public.users CASCADE;
+
+CREATE TABLE public.users (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid (),
+    first_name varchar(225) NOT NULL,
+    middle_name varchar(225),
+    last_name varchar(225) NOT NULL,
+    password varchar(225) NOT NULL,
+    username varchar(225) NOT NULL UNIQUE,
+    role varchar(225) NOT NULL
 );
